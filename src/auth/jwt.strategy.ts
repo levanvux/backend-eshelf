0a17d2dc6b4dda010248c76b49bd10d1e1c9d614
levanvux <hoanngu512@gmail.com>
2026-03-23 16:43:23 +0700

    refactor: replace process.env with ConfigService

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -2,18 +2,20 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Role } from '../../generated/prisma/enums';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
-    if (!process.env['JWT_SECRET']) {
+  constructor(private config: ConfigService) {
+    const jwtSecret = config.get<string>('JWT_SECRET');
+    if (!jwtSecret) {
       throw new Error('JWT_SECRET is not defined');
     }
 
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: process.env['JWT_SECRET'],
+      secretOrKey: jwtSecret,
     });
   }
 
