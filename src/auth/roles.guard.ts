222a17ab9771665b7ac157356815ebfb0bffff26
levanvux <hoanngu512@gmail.com>
2026-03-23 16:06:04 +0700

    feat(auth): Roles guard for role-based access control

---
+++ src/auth/roles.guard.ts
@@ -0,0 +1,23 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLES_KEY } from './roles.decorator';
+import { Role } from 'generated/prisma/enums';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+  constructor(private reflector: Reflector) {}
+
+  canActivate(context: ExecutionContext): boolean {
+    const roles = this.reflector.get<Role[]>(ROLES_KEY, context.getHandler());
+    if (!roles) {
+      return true;
+    }
+    const { user } = context
+      .switchToHttp()
+      .getRequest<{ user: { userId: string; email: string; role: Role } }>();
+    if (!user || !roles.includes(user.role)) {
+      throw new Error('Unauthorized');
+    }
+    return true;
+  }
+}
