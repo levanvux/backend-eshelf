af69a9c2e0744d769b49d8abbac9e15e0e98353b
levanvux <hoanngu512@gmail.com>
2026-03-23 20:41:44 +0700

    chore(prisma): setup prisma service with pg adapter

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,16 +1,18 @@
-import { Injectable, OnModuleInit, OnModuleDestroy } from '@nestjs/common';
-import { PrismaClient } from '../../generated/prisma/client';
+import { Injectable } from '@nestjs/common';
+import { PrismaClient } from '../generated/prisma/client.js';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
-export class PrismaService
-  extends PrismaClient
-  implements OnModuleInit, OnModuleDestroy
-{
-  async onModuleInit() {
-    await this.$connect();
-  }
+export class PrismaService extends PrismaClient {
+  constructor(private config: ConfigService) {
+    const connectionString = config.getOrThrow<string>('DATABASE_URL');
 
-  async onModuleDestroy() {
-    await this.$disconnect();
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+    const adapter = new PrismaPg({
+      connectionString,
+    });
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    super({ adapter });
   }
 }
